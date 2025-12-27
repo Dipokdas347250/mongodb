b9eb4a148d2dfdfd392b90a84794d5389e9583da
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2025-12-27 19:47:48 +0600

    update

--- index.js
+++ index.js
@@ -12,37 +12,80 @@ dbConfig();
 // Connect to the database
 
 // Define a simple route
-app.post('/data', async (req, res) => {
-  let {demo}=req.body;
+ 
+
+  app.post('/data', async (req, res) => {
+    try {
+      let {title, price, description, category, image}=req.body;
   let createTodo = new TodoModel({
-    demo:demo
+    title:title,
+    price:price,
+    description:description,
+    category:category,
+    image:image
   })
   await createTodo.save();
-  res.status(200).json({message: "Data received", data: createTodo});
+  return res.status(200).json({message: "Data received", data: createTodo});
+    } catch (error) {
+      console.log(error);
+  res.status(500).json({message: "Internal Server Error"});
+    }
+  
 });
+  
 
 // read data
 
 app.get("/alltodos", async (req,res)=>{  
 
-  let alltodos = await TodoModel.find({});
+  try {
+     let alltodos = await TodoModel.find({});
 
-  res.status(200).json({success: true, message: "All todos", data: alltodos});
+  return res.status(200).json({success: true, message: "All todos", data: alltodos});
+    
+  } catch (error) {
+    console.log(error);
+  res.status(500).json({message: "Internal Server Error"});
+  }
+
+ 
 
 })
 
 // delete data
 app.delete("/delete/:id", async (req,res)=>{  
-   let {id} = req.params;
+  
+  try {
+    let {id} = req.params;
   let deletedtoto = await TodoModel.findOneAndDelete({_id:id});
 
-  res.status(200).json({success: true, message: "Todo deleted", data: deletedtoto});
+   return res.status(200).json({success: true, message: "Todo deleted", data: deletedtoto});
+  } catch (error) {
+    console.log(error);
+  res.status(500).json({message: "Internal Server Error"});
+  }
+
+   
 
 });
 
 // Start the server
 
+// update data
 
+app.patch("/update/:id", async (req,res)=>{
+  try {
+    let {id} = req.params;
+    // let {title, price, description, category, image}=req.body;
+    let updatedTodo = await TodoModel.findByIdAndUpdate({_id:id}, 
+      req.body,
+     {new: true});
+     return res.status(200).json({success: true, message: "Todo updated", data: updatedTodo});
+  } catch (error) {
+    console.log(error);
+  res.status(500).json({message: "Internal Server Error"});
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`);
--- modle/todo.modle.js
+++ modle/todo.modle.js
@@ -1,11 +1,30 @@
 const mongodb=require("mongoose");
 
-const todoSchema=new mongodb.Schema({
-    demo:{
+const todoSchema=new mongodb.Schema(
+    {
+    title:{
         type:String,
         required:true
       
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String
+        
     }
+
+    
    
 });
 
